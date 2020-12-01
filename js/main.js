3327b3e02fd4aacf21c62d5d36bf0aadc4a18a94
Stefan Kalenderov <stefankalenderov@gmail.com>
2020-12-01 16:51:43 +0200

    added fase fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 !(function($) {
     "use strict";
-  
+
     // Hero typed
     if ($('.typed').length) {
       var typed_strings = $(".typed").data('typed-items');
@@ -13,25 +13,25 @@
         backDelay: 2000
       });
     }
-  
+
     // Smooth scroll for the navigation menu and links with .scrollto classes
     $(document).on('click', '.nav-menu a, .scrollto', function(e) {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         e.preventDefault();
         var target = $(this.hash);
         if (target.length) {
-  
+
           var scrollto = target.offset().top;
-  
+
           $('html, body').animate({
             scrollTop: scrollto
           }, 1500, 'easeInOutExpo');
-  
+
           if ($(this).parents('.nav-menu, .mobile-nav').length) {
             $('.nav-menu .active, .mobile-nav .active').removeClass('active');
             $(this).closest('li').addClass('active');
           }
-  
+
           if ($('body').hasClass('mobile-nav-active')) {
             $('body').removeClass('mobile-nav-active');
             $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
@@ -40,7 +40,7 @@
         }
       }
     });
-  
+
     // Activate smooth scroll on page load with hash links in the url
     $(document).ready(function() {
       if (window.location.hash) {
@@ -53,7 +53,7 @@
         }
       }
     });
-  
+
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
       $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
@@ -74,18 +74,18 @@
         }
       }
     });
-  
+
     // Navigation active state on scroll
     var nav_sections = $('section');
     var main_nav = $('.nav-menu, .mobile-nav');
-  
+
     $(window).on('scroll', function() {
       var cur_pos = $(this).scrollTop() + 200;
-  
+
       nav_sections.each(function() {
         var top = $(this).offset().top,
           bottom = top + $(this).outerHeight();
-  
+
         if (cur_pos >= top && cur_pos <= bottom) {
           if (cur_pos <= bottom) {
             main_nav.find('li').removeClass('active');
@@ -97,7 +97,7 @@
         }
       });
     });
-  
+
     // Back to top button
     $(window).scroll(function() {
       if ($(this).scrollTop() > 100) {
@@ -106,20 +106,20 @@
         $('.back-to-top').fadeOut('slow');
       }
     });
-  
+
     $('.back-to-top').click(function() {
       $('html, body').animate({
         scrollTop: 0
       }, 1500, 'easeInOutExpo');
       return false;
     });
-  
+
     // jQuery counterUp
     $('[data-toggle="counter-up"]').counterUp({
       delay: 10,
       time: 1000
     });
-  
+
     // Skills section
     $('.skills-content').waypoint(function() {
       $('.progress .progress-bar').each(function() {
@@ -128,30 +128,30 @@
     }, {
       offset: '80%'
     });
-  
+
     // Porfolio isotope and filter
     $(window).on('load', function() {
       var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
       });
-  
+
       $('#portfolio-flters li').on('click', function() {
         $("#portfolio-flters li").removeClass('filter-active');
         $(this).addClass('filter-active');
-  
+
         portfolioIsotope.isotope({
           filter: $(this).data('filter')
         });
         aos_init();
       });
-  
+
       // Initiate venobox (lightbox feature used in portofilo)
       $(document).ready(function() {
         $('.venobox').venobox();
       });
     });
-  
+
     // Testimonials carousel (uses the Owl Carousel library)
     $(".testimonials-carousel").owlCarousel({
       autoplay: true,
@@ -169,7 +169,7 @@
         }
       }
     });
-  
+
     // Portfolio details carousel
     $(".portfolio-details-carousel").owlCarousel({
       autoplay: true,
@@ -177,7 +177,7 @@
       loop: true,
       items: 1
     });
-  
+
     // Init AOS
     function aos_init() {
       AOS.init({
@@ -189,5 +189,5 @@
     $(window).on('load', function() {
       aos_init();
     });
-  
+
   })(jQuery);
